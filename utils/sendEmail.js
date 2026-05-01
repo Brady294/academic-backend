84cf00d892a33163d4c0fd4bc06b8ce32982de61
Brady <githinjijohn0294@gmail.com>
2026-05-02 01:11:11 +0300

    Working on the Email Verification

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+    throw new Error("EMAIL_USER or EMAIL_PASS is missing");
+  }
+
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: true,
+    },
+    family: 4,
   });
 
   await transporter.sendMail({
